b7f0c11db9ba41f636e57e407f25dff133d7e1a2
Will Badart <Badart_William@bah.com>
2020-12-07 23:42:55 -0500

    day05b

--- src/Solutions/Day05.hs
+++ src/Solutions/Day05.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings,ViewPatterns #-}
 
 module Solutions.Day05 where
 
@@ -8,21 +8,23 @@ day05a :: NonEmpty String -> Either String Int
 day05a = traverse seatId .> maximum1 >>> maybeToRight "bad parse"
 
 day05b :: NonEmpty String -> Either String Int
-day05b = undefined
+day05b xs = do
+  ids <- maybeToRight "bad parse" (traverse seatId xs >>= nonEmpty . sort . toList)
+  let foo = evalState (traverse go ids) (prev (head ids))
+  maybeToRight "no solution" $ viaNonEmpty (prev . head) $ fmap fst $ filter snd $ zip (toList ids) (toList foo)
+
+go :: (Eq a, Enum a) => a -> State a Bool
+go a = state \(succ -> a') -> (a /= a', a)
 
 seatId :: String -> Maybe Int
 seatId = splitAt 7 >>> (rowNum *** colNum) >>> uncurry (liftA2 (\r c -> r * 8 + c))
-
-rowNum :: String -> Maybe Int
-rowNum = solve (0, 127) 'F' 'B'
-
-colNum :: String -> Maybe Int
-colNum = solve (0, 7) 'L' 'R'
-
-solve :: (Int, Int) -> Char -> Char -> String -> Maybe Int
-solve initRange loChar hiChar = foldlM go initRange .> fst
   where
-    go :: (Int, Int) -> Char -> Maybe (Int, Int)
-    go (lo, hi) c | c == loChar = Just (lo, lo + ((hi - lo) `div` 2))
-                  | c == hiChar = Just ((lo + (hi - lo) `div` 2) + 1, hi)
-                  | otherwise   = Nothing
+    rowNum = solve (0, 127) 'F' 'B'
+    colNum = solve (0, 7) 'L' 'R'
+    solve :: (Int, Int) -> Char -> Char -> String -> Maybe Int
+    solve initRange loChar hiChar = foldlM go initRange .> fst
+      where
+        go :: (Int, Int) -> Char -> Maybe (Int, Int)
+        go (lo, hi) c | c == loChar = Just (lo, lo + ((hi - lo) `div` 2))
+                      | c == hiChar = Just ((lo + (hi - lo) `div` 2) + 1, hi)
+                      | otherwise   = Nothing
