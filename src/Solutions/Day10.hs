e71cd4c36ff2e95f5a4c27042b08a853bbe28880
Will Badart <Badart_William@bah.com>
2020-12-10 12:13:48 -0500

    day10a

--- src/Solutions/Day10.hs
+++ src/Solutions/Day10.hs
@@ -1,7 +1,19 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Solutions.Day10 where
 
 day10a :: NonEmpty String -> Either String Int
-day10a = undefined
+day10a =
+  traverse readMaybe
+  >>> fmap (sort . toList)
+  >>> maybeToRight "bad parse"
+  >>> fmap
+    (foldl' (\(prev :: Int, r) j -> (j, j - prev : r)) (0, [])
+    >>> snd
+    >>> count 1 &&& (+1) . count 3
+    >>> uncurry (*))
+  where
+    count a = foldr (\x c -> if x == a then c + 1 else c) 0
 
 day10b :: NonEmpty String -> Either String Int
 day10b = undefined
